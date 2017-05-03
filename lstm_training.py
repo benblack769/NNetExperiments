7d5d8953ec378711bc86cb0a0399d499c7f2b5db
Ben Black <weepingwillowben@gmail.com>
2017-05-03 16:37:32 -0700

    finalized lstm refractor

---
+++ lstm_training.py
@@ -0,0 +1,32 @@
+import time
+import os
+import plot_utility
+import numpy as np
+import string_processing
+from lstm import *
+
+SEQUENCE_LEN = 50
+BATCH_SIZE = 128
+
+IN_LEN = string_processing.CHARS_LEN
+HIDDEN_LEN = 600
+OUT_LEN = string_processing.CHARS_LEN
+
+layer1 = LSTM_Layer("layer1",IN_LEN,HIDDEN_LEN)
+layer2 = LSTM_Layer("layer2",HIDDEN_LEN,OUT_LEN)
+full_layer = TwoLayerLSTM(layer1,layer2)
+
+optimizer = RMSpropOpt(0.01)
+
+full_layer_learner = Learner(full_layer,RMSpropOpt,optimizer,calc_error_catagorized,BATCH_SIZE,SEQUENCE_LEN)
+
+
+def generate_text_input():
+    train_str = string_processing.get_str("data/huck_fin.txt")
+    in_vec_list = string_processing.in_vec(train_str)
+    in_stack = np.vstack(in_vec_list)
+    return in_stack
+
+text_in = generate_text_input()
+NUM_EPOCS = 10
+train(full_layer_learner,text_in,text_in,NUM_EPOCS)
