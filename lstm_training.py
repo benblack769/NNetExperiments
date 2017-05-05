fa977b5ca3b7b0a36e5097dfa3048cb06a4a35c7
Ben Black <weepingwillowben@gmail.com>
2017-05-04 22:56:31 -0700

    arg

--- lstm_training.py
+++ lstm_training.py
@@ -9,17 +9,17 @@ SEQUENCE_LEN = 50
 BATCH_SIZE = 256
 
 IN_LEN = string_processing.CHARS_LEN
-HIDDEN_LEN_1 = 500
-HIDDEN_LEN_2 = 500
+HIDDEN_LEN_1 = 400
+HIDDEN_LEN_2 = 200
 OUT_LEN = string_processing.CHARS_LEN
 
 layer1 = LSTM_Layer("layer1",IN_LEN,HIDDEN_LEN_1)
-layer2 = LSTM_Layer("layer8",HIDDEN_LEN_1,HIDDEN_LEN_2)
+layer2 = LSTM_Layer("layer9",HIDDEN_LEN_1,HIDDEN_LEN_2)
 layer3 = LSTM_Layer("tanh_layer",HIDDEN_LEN_2,OUT_LEN)
 full_layer = TwoLayerLSTM(TwoLayerLSTM(layer1,layer2),layer3)
 layer_names = [l.name for l in full_layer.get_weight_biases()]
 
-optimizer = RMSpropOpt(0.16)
+optimizer = RMSpropOpt(0.06)
 
 full_layer_learner = Learner(full_layer,optimizer,calc_error_squared,BATCH_SIZE,SEQUENCE_LEN)
 
