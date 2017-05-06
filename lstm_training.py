ab3a488240b85a8b81b830cf47904cf6cce00028
Ben Black <weepingwillowben@gmail.com>
2017-05-06 09:02:37 -0700

    updated nn structure

--- lstm_training.py
+++ lstm_training.py
@@ -18,7 +18,7 @@ layer2 = LSTM_Layer("layer12",HIDDEN_LEN_1,HIDDEN_LEN_2)
 layer3 = TanhLayer("tanh_layer1",HIDDEN_LEN_2,OUT_LEN)
 full_layer = TwoLayerLSTM(TwoLayerLSTM(layer1,layer2),layer3)
 
-optimizer = RMSpropOpt(0.02)
+optimizer = RMSpropOpt(0.01)
 
 full_layer_learner = Learner(full_layer,optimizer,calc_error_catagorized,BATCH_SIZE,SEQUENCE_LEN)
 
